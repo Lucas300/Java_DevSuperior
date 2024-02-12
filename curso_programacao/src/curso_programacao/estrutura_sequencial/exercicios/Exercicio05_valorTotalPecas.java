a76a05903ce3cde4aaa47c70f73b6937aafe3a03
Lucas Daniel <ludaniel.sd@gmail.com>
2024-02-11 23:00:08 -0300

    Exercicio 05 estrtura sequencial - Calculando a soma da compra de 2 itens com base no preço e da quantidade

---
+++ curso_programacao/src/curso_programacao/estrutura_sequencial/exercicios/Exercicio05_valorTotalPecas.java
@@ -0,0 +1,25 @@
+package curso_programacao.estrutura_sequencial.exercicios;
+
+import java.util.Scanner;
+
+public class Exercicio05_valorTotalPecas {
+
+	public static void main(String[] args) {
+		Scanner entrada = new Scanner(System.in);
+		int codigo1 = entrada.nextInt();
+		int quantidade1 = entrada.nextInt();
+		double preco1 = entrada.nextDouble();
+		double total1 = quantidade1 * preco1;
+		
+		int codigo2 = entrada.nextInt();
+		int quantidade2 = entrada.nextInt();
+		double preco2 = entrada.nextDouble();
+		double total2 = quantidade2 * preco2;
+		
+		var total = total1 + total2;
+		System.out.println("Valor a pagar R$:"+total);
+		
+		entrada.close();
+	}
+
+}
