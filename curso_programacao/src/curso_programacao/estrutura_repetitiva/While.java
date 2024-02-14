3d72010a9ed4c3560005b4f188d4b9d831b55cdb
Lucas Daniel <ludaniel.sd@gmail.com>
2024-02-14 19:00:06 -0300

    Estrtura Repetitiva - While (repete enquanto a condição for verdadeira)

---
+++ curso_programacao/src/curso_programacao/estrutura_repetitiva/While.java
@@ -0,0 +1,23 @@
+package curso_programacao.estrutura_repetitiva;
+
+import java.util.Scanner;
+
+public class While {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();;
+		int soma_dos_numeros = 0;
+		
+		//repetir enquanto o n for diferente de 0
+		while(n != 0) {
+			soma_dos_numeros += n;
+			n = sc.nextInt();
+		}
+		System.out.println("A soma dos números digitados é:"+soma_dos_numeros);
+		
+		
+		sc.close();
+	}
+
+}
