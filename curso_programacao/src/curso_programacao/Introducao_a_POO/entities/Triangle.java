bb8dc21aac464f101819ea7d0699896ad09c8a76
Lucas Daniel <ludaniel.sd@gmail.com>
2024-02-19 22:22:54 -0300

    Introdução a POO, Resolvendo o problema da comparação da area dos triangulos e adicionando o POO ao programa

--- curso_programacao/src/curso_programacao/Introducao_a_POO/entities/Triangle.java
+++ curso_programacao/src/curso_programacao/Introducao_a_POO/entities/Triangle.java
@@ -6,5 +6,9 @@ public class Triangle {
 	public double b;
 	public double c;
 	
-	
+	//criando um metodo para calcular a area do triangulo
+	public double area() {
+		double p = (a + b + c)/2;
+		return Math.sqrt(p * (p - a) * (p - b) * (p - c));	
+	}
 }
