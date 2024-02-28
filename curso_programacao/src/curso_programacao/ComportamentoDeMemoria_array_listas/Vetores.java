12cd67702b093e7cacd58476e41500b1a168e029
Lucas Daniel <ludaniel.sd@gmail.com>
2024-02-27 21:38:58 -0300

    Comportamento de memória, arrays, listas - Trabalhando com arrays, fazendo um software de media de alturas(pedindo a quantidade de alturas - que sera a base do tamanho do array)

---
+++ curso_programacao/src/curso_programacao/ComportamentoDeMemoria_array_listas/Vetores.java
@@ -0,0 +1,22 @@
+package curso_programacao.ComportamentoDeMemoria_array_listas;
+
+import java.util.Scanner;
+
+public class Vetores {
+
+	public static void main(String[] args) {
+		Scanner ler = new Scanner(System.in);
+		int n = ler.nextInt();
+		double total = 0;
+		//vetor
+		double[] altura = new double[n];
+		
+		for (int i = 0; i < n; i++) {
+			altura[i] = ler.nextDouble();
+			total = total + altura[i];
+		}
+		double media = total / n;
+		System.out.printf("Average height = %.2f", media);
+	}
+
+}
