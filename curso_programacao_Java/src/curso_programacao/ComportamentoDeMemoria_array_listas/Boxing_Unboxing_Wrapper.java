8463c3c8ba9a1c1df7c71abeb9576c0941834511
Lucas Daniel <ludaniel.sd@gmail.com>
2024-03-04 21:40:13 -0300

    Comportamento de memória, arrays, Aprendendo sobre Boxing , unboxing e wrapper classes

---
+++ curso_programacao_Java/src/curso_programacao/ComportamentoDeMemoria_array_listas/Boxing_Unboxing_Wrapper.java
@@ -0,0 +1,23 @@
+package curso_programacao.ComportamentoDeMemoria_array_listas;
+
+public class Boxing_Unboxing_Wrapper{
+	public static void main(String[] args) {
+		//Boxing
+		//É o processo de conversão de um objeto tipo valor para um objeto tipo referência compativel
+		int x = 20;
+		Object obj = x;
+		
+		//unboxing
+		//É o processo de convsão de um objeto tipo referência para um objeto tipo valor compatível
+		int y = (int) obj; // recebendo o valor em uma variavel 
+		
+		//Wrappers classes 
+		//são classes equivalentes aos tipos primitivos 
+		//usamos para fazer boxing e unboxing
+		
+		Integer obj2 = x ; //não precisa fazer o casting
+		//usufruem dos recursos do POO
+		
+		
+		}
+	}
