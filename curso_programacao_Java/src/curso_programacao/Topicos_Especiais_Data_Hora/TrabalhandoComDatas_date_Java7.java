10cf88e673cc40df3dea880516190b4b0e5d06b0
Lucas Daniel <ludaniel.sd@gmail.com>
2024-03-09 23:46:06 -0300

    Tópicos especiais em Java, Exbindo a data de uma maneia mais amigavel ao Brasileiro  Exemplo: sábado, 09 de março de 2024

---
+++ curso_programacao_Java/src/curso_programacao/Topicos_Especiais_Data_Hora/TrabalhandoComDatas_date_Java7.java
@@ -0,0 +1,21 @@
+package curso_programacao.Topicos_Especiais_Data_Hora;
+
+	import java.time.LocalDate;
+	import java.time.LocalDateTime;
+	import java.time.format.DateTimeFormatter;
+	import java.util.Locale;
+
+	public class TrabalhandoComDatas_date_Java7 {
+
+	    public static void main(String[] args) {
+	        DateTimeFormatter formatter1 = DateTimeFormatter.ofPattern("EEEE, dd 'de' MMMM 'de' yyyy", new Locale("pt", "BR"));
+	        DateTimeFormatter formatter2 = DateTimeFormatter.ofPattern("EEEE, dd 'de' MMMM 'de' yyyy HH:mm:ss", new Locale("pt", "BR"));
+
+	        LocalDate date1 = LocalDate.parse("09/03/2024", DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+	        LocalDateTime date2 = LocalDateTime.parse("09/03/2024 23:44:07", DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss"));
+
+	        System.out.println(date1.format(formatter1));
+	        System.out.println(date2.format(formatter2));
+	    }
+	}
+
