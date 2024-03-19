bb43f081e2d19529c8fa85aa0c0f6f7b0ce631eb
Lucas Daniel <ludaniel.sd@gmail.com>
2024-03-19 00:44:42 -0300

    Enumeração e composição, fazendo o exercicio de composição parte 2 - incluindo os metodos das classes Worker e HourContract

--- curso_programacao_Java/src/curso_programacao/Enumeracao_Composicao/Exercicio_Composicao/Worker.java
+++ curso_programacao_Java/src/curso_programacao/Enumeracao_Composicao/Exercicio_Composicao/Worker.java
@@ -1,6 +1,7 @@
 package curso_programacao.Enumeracao_Composicao.Exercicio_Composicao;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 public class Worker {
@@ -11,7 +12,7 @@ public class Worker {
 	//O trabalhador pertence a um departamento apenas 
 	private Department department;
 	
-	//Um funcionario pode ter varios contratos
+	//Um funcionario pode ter varios contratos           IMPORTANTE: NÃO USAOS LISTAS NO CONSTRUTOR
 	private List<HourContract> contracts = new ArrayList<>();
 	
 	public Worker() {
@@ -24,6 +25,69 @@ public class Worker {
 		this.baseSalary = baseSalary;
 		this.department = department;
 	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public WorkerLevel getLevel() {
+		return level;
+	}
+
+	public void setLevel(WorkerLevel level) {
+		this.level = level;
+	}
+
+	public Double getBaseSalary() {
+		return baseSalary;
+	}
+
+	public void setBaseSalary(Double baseSalary) {
+		this.baseSalary = baseSalary;
+	}
+
+	public Department getDepartment() {
+		return department;
+	}
+
+	public void setDepartment(Department department) {
+		this.department = department;
+	}
+
+	public List<HourContract> getContracts() {
+		return contracts;
+	}
+	
+	//Adicionando contratos
+	public void addContract(HourContract contract) {
+		contracts.add(contract);
+	}
+	//Removendo contratos
+	public void removeContract(HourContract contract) {
+		contracts.remove(contract);
+	}
+	
+	//implementação do metodo que terona a soma do salario mais a soma do contrato de um determinado mes, usando o calendar
+	public double income(int year,int month) {
+		double sum = baseSalary;
+		//usando o calendar para receber os valores da data dos contratos feito pelo usuario
+		Calendar cal = Calendar.getInstance();
+		for (HourContract c : contracts) {
+			cal.setTime(c.getDate());
+			int c_year = cal.get(Calendar.YEAR);
+		    int c_month = 1 + cal.get(Calendar.MONTH);
+			if (year == c_year && month == c_month) {
+				sum += c.totalValue();
+			}
+		}
+		return sum;
+	}
+	
+	
 	
 	
 }
