8f48fcaeedf94b8b6eed82646155a3ec6f1915ea
Lucas Daniel <ludaniel.sd@gmail.com>
2024-03-21 00:16:19 -0300

    Fazendo um programa que tem uma classe Post e nesse post tem uma outra classe que compõe chamada Comment - Parte 2 Trabalhando no Program onde é montada a parte que sera exibida ao usuário - Trabalhando com StringBuilder para concatenar Strings de maneira correta

--- curso_programacao_Java/src/curso_programacao/Enumeracao_Composicao/Exercicio_Post_Comentarios/Post.java
+++ curso_programacao_Java/src/curso_programacao/Enumeracao_Composicao/Exercicio_Post_Comentarios/Post.java
@@ -1,78 +1,91 @@
 package curso_programacao.Enumeracao_Composicao.Exercicio_Post_Comentarios;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
 public class Post {
-private Date moment;
-private String title;
-private String content;
-private Integer likes;
-
-List<Comment> comments = new ArrayList<Comment>();
-
-public Post() {
+	private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
 	
-}
+	private Date moment;
+	private String title;
+	private String content;
+	private Integer likes;
 
-public Post(Date moment, String title, String content, Integer likes) {
-	this.moment = moment;
-	this.title = title;
-	this.content = content;
-	this.likes = likes;
-}
+	List<Comment> comments = new ArrayList<Comment>();
 
-public Date getMoment() {
-	return moment;
-}
+	public Post() {
 
-public void setMoment(Date moment) {
-	this.moment = moment;
-}
+	}
 
-public String getTitle() {
-	return title;
-}
+	public Post(Date moment, String title, String content, Integer likes) {
+		this.moment = moment;
+		this.title = title;
+		this.content = content;
+		this.likes = likes;
+	}
 
-public void setTitle(String title) {
-	this.title = title;
-}
+	public Date getMoment() {
+		return moment;
+	}
 
-public String getContent() {
-	return content;
-}
+	public void setMoment(Date moment) {
+		this.moment = moment;
+	}
 
-public void setContent(String content) {
-	this.content = content;
-}
+	public String getTitle() {
+		return title;
+	}
 
-public Integer getLikes() {
-	return likes;
-}
+	public void setTitle(String title) {
+		this.title = title;
+	}
 
-public void setLikes(Integer likes) {
-	this.likes = likes;
-}
+	public String getContent() {
+		return content;
+	}
 
-public List<Comment> getComments() {
-	return comments;
-}
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public Integer getLikes() {
+		return likes;
+	}
+
+	public void setLikes(Integer likes) {
+		this.likes = likes;
+	}
+
+	public List<Comment> getComments() {
+		return comments;
+	}
 //excluir o setCommets
 
+	public void addComment(Comment comment) {
+		comments.add(comment);
+	}
 
-public void addComment(Comment comment) {
-	comments.add(comment);
-}
+	public void removeComment(Comment comment) {
+		comments.remove(comment);
+	}
 
-public void removeComment(Comment comment) {
-	comments.remove(comment);
-}
-
-
-
-
-
-
-
+	//Usamos o String builder para retornos maiores 
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		//append siginifica acrescentar no final , usado pra concatenar Strings
+		sb.append(title + "\n");
+		sb.append(likes);
+		sb.append(" Likes - ");
+		sb.append(sdf.format(moment)+"\n");
+		sb.append(content + "\n");
+		sb.append("Comments:\n");
+		for (Comment c: comments) {
+			sb.append(c.getText()+"\n");
+		}
+		return sb.toString();
+		
+		
+	}
 }
--- curso_programacao_Java/src/curso_programacao/Enumeracao_Composicao/Exercicio_Post_Comentarios/Program.java
+++ curso_programacao_Java/src/curso_programacao/Enumeracao_Composicao/Exercicio_Post_Comentarios/Program.java
@@ -1,5 +1,30 @@
 package curso_programacao.Enumeracao_Composicao.Exercicio_Post_Comentarios;
 
-public class Program {
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
+public class Program {
+	@SuppressWarnings("unused")
+	public static void main(String[] args) throws ParseException {
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+		
+		Comment c1 = new Comment("Have a nice trip ");
+		Comment c2 = new Comment("Wow thats awesome!");
+		Post p1 = new Post(sdf.parse("21/06/2018 13:05:44"),"Traveling to New Zealand","Im going to visit this wonderful country!", 12);
+		//ligando os comentarios ao post
+		p1.addComment(c1);
+		p1.addComment(c2);
+		
+		
+		Comment c3 = new Comment("Good night");
+		Comment c4 = new Comment("May the Force be with you");
+		Post p2 = new Post(sdf.parse("28/07/2018 23:14:19"),"Good night guys","See you tomorrow",5);
+		p2.addComment(c3);
+		p2.addComment(c4);
+		
+		System.out.println(p1);
+		System.out.println(p2);
+	
+	}
 }
