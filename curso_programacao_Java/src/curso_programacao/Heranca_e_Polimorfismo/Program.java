7b624d2982e0237214ae64cbc0e060c100c75e11
Lucas Daniel <ludaniel.sd@gmail.com>
2024-04-03 23:43:12 -0300

    Trabalhando com Upcasting e Downcasting, principios de Polimorfismo

--- curso_programacao_Java/src/curso_programacao/Heranca_e_Polimorfismo/Program.java
+++ curso_programacao_Java/src/curso_programacao/Heranca_e_Polimorfismo/Program.java
@@ -2,10 +2,39 @@ package curso_programacao.Heranca_e_Polimorfismo;
 
 public class Program {
 
+	@SuppressWarnings("unused")
 	public static void main(String[] args) {
-		
-		
 
+		Account acc = new Account(1001,"alex",0.0);
+		
+		BusinessAccount bacc = new BusinessAccount(1002, "Maria", 0.0,500.0);
+		
+		//UPCASTING
+		Account acc1 = bacc;
+		Account acc2 = new BusinessAccount(1003, "Bob", 0.0, 200.00);
+		Account acc3 = new SavingsAccount(1004, "Anna", 0.0, 0.01);
+		
+		
+		//DOWNCASTING
+		BusinessAccount acc4 = (BusinessAccount)acc2;
+		acc4.loan(100.0);
+		
+		//vai dar um erro porque  o acc3 é SavingsAccount
+		//BusinessAccount acc5 = (BusinessAccount)acc3;
+		
+		//verificando se é instancia
+		if(acc3 instanceof BusinessAccount) {
+			BusinessAccount acc5 = (BusinessAccount)acc3;
+			acc5.loan(200.0);
+			System.out.println("Loan!");
+		}
+		
+		if(acc3 instanceof SavingsAccount) {
+			SavingsAccount acc5 =(SavingsAccount)acc3;
+			acc5.updateBalance();
+			System.out.println("Update!");
+		}
+		
 	}
 
 }
